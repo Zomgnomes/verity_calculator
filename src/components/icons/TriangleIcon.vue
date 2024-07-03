60436f492b03e2a0eed421cb7854e03fdfc39e53
Alex Smith <z@omg.nom.es>
2024-07-03 01:04:11 -0400

    First version in vue

---
+++ src/components/icons/TriangleIcon.vue
@@ -0,0 +1,7 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 404.4 404.4">
+    <path style="fill: currentColor; stroke-width: 0; stroke-miterlimit: 1;"
+          d="M346.3 342.2H58.1L202.2 56.1l144.1 286.1Zm-272-10h255.8L202.2 78.3 74.3 332.2Z"
+    />
+  </svg>
+</template>
---
+++ src/types.ts
@@ -0,0 +1,23 @@
+export type shape2d = "C" | "S" | "T";
+
+export type shape3d = "CC" | "CS" | "CT" | "SS" | "ST" | "TT";
+
+export type allShapes = shape2d | shape3d
+
+export type shapePosition = "L" | "M" | "R";
+
+export enum shapeState {
+    disabled = 0,
+    selected = 1,
+    error = 2,
+}
+
+export type shape2dSelections = {
+    "shape":shape2d|null,
+    "state":shapeState|null,
+}
+
+export type shape3dSelections = {
+    "shape":shape3d|null,
+    "state":shapeState|null,
+}
